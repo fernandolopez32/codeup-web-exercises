1ad9d19f8e1ad16c2d76d1f428e72f99dbc85b1e
Fernando Lopez <flopez00032@gmail.com>
2022-08-12 13:08:32 -0500

    external-js-exercises

---
+++ js/javascript-with-html.js
@@ -0,0 +1,18 @@
+//alert("hello")
+
+
+// confirm dialogue
+// let userChoice = confirm("are you sure you want to exit");
+//
+// if (userChoice) {
+//     alert("yay");
+//     }else {
+//         alert ("boooo");
+//     }
+
+// PROMPT
+// let name=prompt("what is you name?");
+// console.log(name);
+
+
+
