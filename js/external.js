7fcdeb0a6c22799808bf727e63f62062f8a79f88
Fernando Lopez <flopez00032@gmail.com>
2022-08-13 10:01:59 -0500

    exercise 4 username-pword

--- js/external.js
+++ js/external.js
@@ -1,53 +1,58 @@
-console.log("Hello from external JavaScript")
-alert("Welcome to my website!")
+// console.log("Hello from external JavaScript")
+// alert("Welcome to my website!")
+//
+// let favoriteColor = prompt("What is your favorite color?")
+// if (favoriteColor){
+//     alert("Great, " + favoriteColor + " is my favorite color too!")
+//     }
+//
+//  //*************EXERCISE 3*************
+//  //SCENARIO 1
+// let daysOfLittleMermaind = prompt("how many days did you rent little Mermaid");
+// let daysOfBrotherBear = prompt("how many days of brother bear")
+// let daysOfHercules = prompt ("how many days of Hercules ")
+//
+// let totalCost = (daysOfLittleMermaind * 3 ) + (daysOfBrotherBear * 3) + (daysOfHercules * 3);
+//     alert(totalCost +" Dollars is your total cost");
+//
+// //SCENARIO 2
+// let hoursAtGoogle = prompt("How many hours did you work at google");
+// let payAtGoogle = prompt ("what is your hourly rate at google?");
+// let hoursAtAmazon = prompt("hours at amazon")
+// let payAtAmazon = prompt("pay rate at amazon")
+// let hoursAtFacebook = prompt("hours at google")
+// let payAtFacebook = prompt("pay rate at amazon")
+//
+// let totalPay = hoursAtAmazon* payAtAmazon + hoursAtGoogle*payAtGoogle + hoursAtFacebook * payAtFacebook;
+//     alert ("your total pay is " + totalPay+ " dollars")
+//
+// //SCENARIO 3
+//
+// let classNotFull = confirm("is class no full?");
+// let noScheduleConflict = confirm("is there no conflict");
+// let canEnroll = classNotFull && noScheduleConflict;
+//
+// if (canEnroll){
+//     alert("you can enroll:");
+// }else {
+//     alert("you cant enroll");
+// }
+// //SCENARIO 4
+//
+// let numberOfItemsBought = prompt("how many items were purchased");
+// let offerExpired = confirm("has the offer expired?");
+// let premiumMember = confirm("is the customer premium member?");
+// let makeProductOffer = (premiumMember || numberOfItemsBought > 2) &&
+//     !offerExpired;
+//
+// if (makeProductOffer) {
+//     alert("you bet a product offer ");
+// } else {
+//     alert("you are broke, sorry.");
+// }
+// //**********EXERCISE 4**************
 
-let favoriteColor = prompt("What is your favorite color?")
-if (favoriteColor){
-    alert("Great, " + favoriteColor + " is my favorite color too!")
-    }
+var username = 'codeup';
+var password = 'notastrongpassword';
 
- //*************EXERCISE 3*************
- //SCENARIO 1
-let daysOfLittleMermaind = prompt("how many days did you rent little Mermaid");
-let daysOfBrotherBear = prompt("how many days of brother bear")
-let daysOfHercules = prompt ("how many days of Hercules ")
-
-let totalCost = (daysOfLittleMermaind * 3 ) + (daysOfBrotherBear * 3) + (daysOfHercules * 3);
-    alert(totalCost +" Dollars is your total cost");
-
-//SCENARIO 2
-let hoursAtGoogle = prompt("How many hours did you work at google");
-let payAtGoogle = prompt ("what is your hourly rate at google?");
-let hoursAtAmazon = prompt("hours at amazon")
-let payAtAmazon = prompt("pay rate at amazon")
-let hoursAtFacebook = prompt("hours at google")
-let payAtFacebook = prompt("pay rate at amazon")
-
-let totalPay = hoursAtAmazon* payAtAmazon + hoursAtGoogle*payAtGoogle + hoursAtFacebook * payAtFacebook;
-    alert ("your total pay is " + totalPay+ " dollars")
-
-//SCENARIO 3
-
-let classNotFull = confirm("is class no full?");
-let noScheduleConflict = confirm("is there no conflict");
-let canEnroll = classNotFull && noScheduleConflict;
-
-if (canEnroll){
-    alert("you can enroll:");
-}else {
-    alert("you cant enroll");
-}
-//SCENARIO 4
-
-let numberOfItemsBought = prompt("how many items were purchased");
-let offerExpired = confirm("has the offer expired?");
-let premiumMember = confirm("is the customer premium member?");
-let makeProductOffer = (premiumMember || numberOfItemsBought > 2) &&
-    !offerExpired;
-
-if (makeProductOffer) {
-    alert("you bet a product offer ");
-} else {
-    alert("you are broke, sorry.");
-}
-
+alert(Boolean(password.length >= 5));
