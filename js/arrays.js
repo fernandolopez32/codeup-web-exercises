bb7e53bfbeaa12c1124a0bb2a8326e91623446aa
Fernando Lopez <flopez00032@gmail.com>
2022-08-19 11:38:22 -0500

    before review Iterating arrays

--- js/arrays.js
+++ js/arrays.js
@@ -1,12 +1,59 @@
 
 // declaring an array literal:
 // each item in an array is called an element and are zero index
-let groceryList = ['bread', 'eggs','butter', 'milk']
+// let groceryList = ['bread', 'eggs','butter', 'milk']
+//
+// for (let i=0; i < groceryList.length; i++) {
+//     if (i === groceryList.length - 1) {
+//         console.log("I have to buy " + groceryList[i] + ".");
+//     } else {
+//         console.log("I have to buy " + groceryList[i] + " and");
+//     }
+// }
+// let myArray = ['pizza', 'burgers', 'bananas', 'apples']
+//
+// for (let i = 0; i <myArray.length; i++) {
+//     if (i === myArray.length - 4) {
+//         console.log("I like " + myArray[i] + " because of TMNT!");
+//     } else  {
+//         console.log(`First things that came to my mind ` + myArray[i]);
+//     }
+// }
 
-for (let i=0; i < groceryList.length; i++) {
-    if (i === groceryList.length - 1) {
-        console.log("I have to buy " + groceryList[i] + ".");
-    } else {
-        console.log("I have to buy " + groceryList[i] + " and");
-    }
+let texasCities = ['San Antonio','Houston', 'Dallas', 'El Paso'];
+// // for (let i = 0; i < texasCities.length; i++) {
+// //     if (i % 2 === 1) {
+// //         console.log(texasCities[i] + " is an odd texas city")
+// //     } else {
+// //         console.log(texasCities[i]);
+// //     }
+// // }
+//
+// function outputArray (array){
+//     for (let i = 0; i < array.length; i++){
+//         console.log(array[i]);
+//     }
+// }
+// outputArray(texasCities);
+
+// forEach loop takes an anonymous function
+// the function that we pass to the forEach loop has up to three parameters
+// the first parameter represents the arrays elements
+// the second parameter represents the element index
+//third parameter represents the array itself
+// the second and third parameters are optional
+
+let prices = [32.99, 21.99, 6.99,4.99,12.99,8.98, 5.99];
+
+prices.forEach(function (price, index){
+    console.log("item number " + index + " cost "  +price);
+});
+function x(para1, para2){
+
 }
+//
+// const x = (para1, para2) =>
+
+
+// since the functions are anonymous we can use arrow sintax
+// prices.forEach((price,index) => return  )
---
+++ js/iterating.js
@@ -0,0 +1,63 @@
+let names = ['fernando', 'rodrigo', 'david', 'arron'];
+
+names.forEach(function(names,index){
+    console.log(names +" "+index);
+
+    "use strict";
+
+    /**
+     * TODO:
+     * Create an array of 4 people's names and store it in a variable called
+     * 'names'.
+     */
+
+    /**
+     * TODO:
+     * Create a log statement that will log the number of elements in the names
+     * array.
+     */
+
+    /**
+     * TODO:
+     * Create log statements that will print each of the names individually by
+     * accessing each element's index.
+     */
+
+    /**
+     * TODO:
+     * Write some code that uses a for loop to log every item in the names
+     * array.
+     */
+
+    /**
+     * TODO:
+     * Refactor your above code to use a `forEach` loop
+     */
+
+    /**
+     * TODO:
+     * Create the following three functions, each will accept an array and
+     * return an an element from it
+     * - first: returns the first item in the array
+     * - second: returns the second item in the array
+     * - last: returns the last item in the array
+     *
+     * Example:
+     *  > first([1, 2, 3, 4, 5]) // returns 1
+     *  > second([1, 2, 3, 4, 5]) // returns 2
+     *  > last([1, 2, 3, 4, 5]) // return 5
+     */
+});
+
+let numbers = [1,2,3,4];
+
+function first (number){
+   return number[0] ;
+}
+
+function second (number){
+    return number[1] ;
+}
+function last (number){
+    return number[number.length -1] ;
+}
