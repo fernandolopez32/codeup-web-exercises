b1c6c92c19e176ea3d258f8fa28700bb4daea1a0
Fernando Lopez <flopez00032@gmail.com>
2022-08-29 16:42:17 -0500

    pyramid funcion

--- js/warmups.js
+++ js/warmups.js
@@ -126,7 +126,9 @@ function isDesirable(neighborhoodObject){
     }
     return neighborhoodObject.medianHomePrice<300000 && neighborhoodObject.crimeRate === "low"&& total >=24;
 }
-//Let's pretend your company just hired your friend from college and paid you a referral bonus. Awesome! To celebrate, you're taking your team out to the terrible dive bar next door and using the referral bonus to buy, and build, the largest three-dimensional beer can pyramid you can. And then probably drink those beers, because let's pretend it's Friday too.
+//Let's pretend your company just hired your friend from college and paid you a referral bonus. Awesome! To celebrate,
+// you're taking your team out to the terrible dive bar next door and using the referral bonus to buy, and build,
+// the largest three-dimensional beer can pyramid you can. And then probably drink those beers, because let's pretend it's Friday too.
 //
 // A beer can pyramid will square the number of cans in each level - 1 can in the top level, 4 in the second, 9 in the next, 16, 25...
 //
@@ -140,3 +142,21 @@ function isDesirable(neighborhoodObject){
 //
 // beeramid(1500, 2); // should === 12
 // beeramid(5000, 3); // should === 16
+
+// what's the cost of each level? level*level*priceOfCan
+
+// I need a totalCost accumulator variable and some king of level variable
+// and keep looping util total cost is grater that the bonus
+
+function beeramid(bonus, price){
+    let totalCost = 0;
+    let levels = 0;
+    while (totalCost <= bonus){
+        totalCost += levels * levels *price;
+        if(totalCost + ((levels +1) **2 * price) >bonus){break;}
+        levels ++;
+    }
+    return levels;
+}
+
+
