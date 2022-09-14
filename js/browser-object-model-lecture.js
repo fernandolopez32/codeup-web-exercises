d4fcb8666cf7e70cb45be6e333f0436c440ac8b5
Fernando Lopez <flopez00032@gmail.com>
2022-09-14 14:15:29 -0500

    BOM lecture complete

---
+++ js/browser-object-model-lecture.js
@@ -0,0 +1,58 @@
+
+// let sayHey = function() {
+//     let count = 0;
+//
+//     function hey() {
+//         if (count < 10) {
+//             console.log('hey ' + count);
+//             count++;
+//         } else {
+//             clearInterval(theInterval);
+//         }
+//     }
+//
+//     let theInterval = setInterval(hey, 1000);
+// }
+
+// let count = 0
+// let thisTimeItWillSto;
+// function hey(){
+//     if(count < 10){
+//         console.log("hey "+count);
+//         count++;
+//     }else{
+//         clearInterval(thisTimeItWillSto);
+//     }
+// }
+// thisTimeItWillSto = setInterval(hey,1000);
+
+
+
+// function tenHeys(){
+//     let count = 0 ;
+//     let thisWillStop = setInterval(function (){
+//         if(count < 10) {
+//             console.log("hey " + count);
+//             count++;
+//         }else{
+//             clearInterval(thisWillStop);
+//         }
+//     },1000);
+// }
+
+function tenHeys(){
+    let count = 0;
+    let thisWillStop = setInterval(hey=>{
+        if(count < 10) {
+            console.log("hey " + count);
+            count++;
+        }else{
+            clearInterval(thisWillStop);
+        }
+    },1000);
+}
+
+setTimeout(wakeUpUser, 5000);
+function wakeUpUser(){
+    alert("WAKE UUUUUUUP!!!!");
+}
