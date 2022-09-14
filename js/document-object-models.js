6e8ba09b062a72326913950836186967ae562fad
Fernando Lopez <flopez00032@gmail.com>
2022-09-14 17:01:22 -0500

    DOM letcure for loop

--- js/document-object-models.js
+++ js/document-object-models.js
@@ -1,21 +1,32 @@
-let accessTheDOM = document.getElementById("dc");
-let showMe = accessTheDOM.innerHTML;
-console.log(showMe);
+// let accessTheDOM = document.getElementById("dc");
+// let showMe = accessTheDOM.innerHTML;
+// console.log(showMe);
+//
+// let preference = "dc" ;
+//
+// let marvel = document.getElementById("marvel");
+//
+// let like = document.getElementById("like");
+// let dontLike = document.getElementById("dontLike");
+// let output = `${dontLike.innerHTML} ${document.getElementById(preference).innerHTML} movies`;
+// console.log(output);
+//
+// accessTheDOM.innerHTML =  marvel.innerHTML;
+//
+// marvel.setAttribute('class','hotpink');
+//
+// let hotPicks = document.getElementsByClassName('hotpink');
+//
+// hotPicks[1].style.fontSize = '5rem';
+//
 
-let preference = "dc" ;
+let submitButton = document.getElementById("colorSubmission");
 
-let marvel = document.getElementById("marvel");
-
-let like = document.getElementById("like");
-let dontLike = document.getElementById("dontLike");
-let output = `${dontLike.innerHTML} ${document.getElementById(preference).innerHTML} movies`;
-console.log(output);
-
-accessTheDOM.innerHTML =  marvel.innerHTML;
-
-marvel.setAttribute('class','hotpink');
-
-let hotPicks = document.getElementsByClassName('hotpink');
-
-hotPicks[1].style.fontSize = '5rem';
-
+submitButton.onclick = function (){
+    let colorInput = document.getElementById("colorPreference");
+    let paragraphs = document.getElementsByTagName("p");
+    for(let i = 0; i < paragraphs.length; i++){
+        paragraphs[i].style.color =
+            colorInput.value;
+    }
+}
