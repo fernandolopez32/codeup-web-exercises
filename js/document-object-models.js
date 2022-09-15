80f0cb50c4764b02bf4bc4496983b83821da755b
Fernando Lopez <flopez00032@gmail.com>
2022-09-15 10:38:02 -0500

    dom lecture changing values of dataset

--- js/document-object-models.js
+++ js/document-object-models.js
@@ -30,9 +30,38 @@ submitButton.onclick = function (){
     }
 }
 
+
+// reference to the element on the page
+let theHulk = document.getElementById("hulk");
+// let hulkState = theHulk.getAttribute("data-state");
+let hulkStrength = theHulk.dataset.strength;
+let hulkState = theHulk.dataset.state;
+let hulkIntelligence = theHulk.dataset.intelligence;
+let hulkSummary =` The Hulk is in ${hulkState} state. His strength is ${hulkStrength} and his intelligance is ${hulkIntelligence}.`
+// theHulk.innerText = hulkSummary
+
+let transformButton = document.getElementById("transformHulk");
+transformButton.onclick = function (){
+    let theHulk = document.getElementById("hulk");
+    let hulkStrength = theHulk.dataset.strength;
+    let hulkState = theHulk.dataset.state;
+    let hulkIntelligence = theHulk.dataset.intelligence;
+    if(hulkState === "hulk"){
+        theHulk.dataset.state = "Bruce Banner";
+    } else{
+        theHulk.dataset.state = "hulk";
+    }
+    theHulk.innerText = `The Hulk is in ${hulkState} state.`
+
+
+}
+
+
 //intent of changing the submit button on mouse over
 // submitButton.onmouseover = function (){
 //     let form = document.getElementsByName("changeColor");
 //     form =
 //
 // }
+
+
