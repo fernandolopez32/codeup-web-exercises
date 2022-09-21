0b8d0987eb7b7348f02adec23e401bb3c926e01e
Fernando Lopez <flopez00032@gmail.com>
2022-09-21 10:42:50 -0500

    jQuery selectors mouse events

--- js/jquery-selectors-lecture.js
+++ js/jquery-selectors-lecture.js
@@ -1,6 +1,21 @@
 
 // the new #(document).ready
 $(function (){
-    $('#library').text("jQuery makes life easier");
+    const $refToLibrary = $('#library')
+    //ID selector to alter content
+    //  $refToLibrary.text("jQuery makes life easier");
+    // //
+    // console.log($refToLibrary.text());
+    let li3Text = $refToLibrary.text();
+    $refToLibrary.text(li3Text + " makes life easier");
+
+    // class selectors and css methods to alert styles
+    $(".framework").css({
+        'border': '1px solid black',
+        'width': 'fit-content'
+    });
+    // type list with chained mothods
+    $("p, li").text("i've taken over.").css("color", "hotpink");
+
 })
 
