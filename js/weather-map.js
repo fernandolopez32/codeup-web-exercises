7cdeac47cf8fe1048ab4e879857adaa068f8ea99
Fernando Lopez <flopez00032@gmail.com>
2022-09-28 12:15:54 -0500

    current weather not working

--- js/weather-map.js
+++ js/weather-map.js
@@ -1,6 +1,6 @@
 $(function (){
     // map display on screen
-    mapboxgl.accessToken = 'pk.eyJ1IjoiZmVybmFuZG9sb3BleiIsImEiOiJjbDhlcHBtYncwdXh0M3ZrOTkzcXVkYTJ1In0.U9pGhWUgH8WGFMATBAhcXg';
+    mapboxgl.accessToken = MAPBOX_API_TOKEN;
     const map = new mapboxgl.Map({
         container: 'map', // container ID
         style: 'mapbox://styles/mapbox/streets-v11', // style URL
@@ -19,8 +19,6 @@ $(function (){
         units: "imperial"
     }).done(function(data) {
         console.log(data)
-        console.log(data.weather[0].main)
-
         /* this is the end of WEATHER API */
     });
 
@@ -34,7 +32,7 @@ $(function (){
             map.setCenter(coordinates);
             updateWeather(coordinates);
         })
-    })
+    });
 
     function printWeather(data){
         $('.container').empty();
@@ -43,16 +41,15 @@ $(function (){
                 // console.log(forecast.weather[0].icon);
                 $('section').append(`
         <div class="card col-8 col-md-3 mt-4 mx-auto my-3 px-0">
-            <div class="card-header p-3">${data.list[index].dt_txt.split(" ")[0]}</div>
+            <div class="card-header p-3">$forecast[index].dt_txt.split(" ")[0]}</div>
             <div class="card-body">
                 <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png" alt="weather symbol"> 
-                <p>Desctiption: ${data.list[index].weather[0].description}</p>
+                <p>Desctiption: ${forecast[index].weather[0].description}</p>
             </div>
-           
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">Humidity: ${data.list[index].main.humidity}%</li>
-                <li class="list-group-item">Wind: ${data.list[index].wind.speed} MPH</li>
-                <li class="list-group-item">Pressure: ${data.list[index].main.pressure} </li>
+                <li class="list-group-item">Humidity: ${forecast[index].main.humidity}%</li>
+                <li class="list-group-item">Wind: ${forecast[index].wind.speed} MPH</li>
+                <li class="list-group-item">Pressure: ${forecast[index].main.pressure} </li>
             </ul>
         </div>`)
         });
@@ -65,6 +62,7 @@ $(function (){
             lon:   coordinates[0],
             units: "imperial"
         }).done(function(data) {
+            console.log(data.city.name)
             $('#currentCity').text(`Current city: ${data.city.name}`);
 
             printWeather(data)
