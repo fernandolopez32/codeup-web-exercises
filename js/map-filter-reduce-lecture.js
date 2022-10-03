dac05814ad995ce59b43f0d541f620fe45905b17
Fernando Lopez <flopez00032@gmail.com>
2022-10-03 14:49:12 -0500

    map filter reduce review

--- js/map-filter-reduce-lecture.js
+++ js/map-filter-reduce-lecture.js
@@ -61,8 +61,8 @@
         }
     ];
 
-    const mileages = cars.map(car => car.mileage);
-    console.log(mileages);
+    // const mileages = cars.map(car => car.mileage);
+    // console.log(mileages);
 
 
     const newObject = cars.map(car => {
@@ -119,8 +119,171 @@
 
     console.log('You have 156.39 in your shopping cart.')
 
-    const total = prices.reduce((acc,curr) =>{
-        return acc + curr;
-    },156.39)
+    // const total = prices.reduce((acc,curr) =>{
+    //     return acc + curr;
+    // },156.39)
+    //
+    // console.log(`Your new total is now ${total.toFixed(2)}`);
 
-    console.log(`Your new total is now ${total.toFixed(2)}`);
+
+    const mileages = cars.reduce((acc ,car) => {
+        acc.push(car.mileage);
+        return acc;
+    },[]);
+
+    console.log(mileages);
+
+    // const highestmileage = cars.reduce((acc ,car,i,array) => {
+    //     acc.push(car.mileage);
+    //     if (i===array.length -1){
+    //         return acc.sort((a,b)=> b-a)[0];
+    //     }
+    //     return acc;
+    // },[])
+
+
+    const highestmileage = cars.reduce((acc ,car,i,array) => {
+        acc.push(car.mileage);
+            return acc;
+    },[]).reduce((acc, mlgs) => {
+            return Math.max(acc,mlgs);
+    });
+
+    // const highestmileage = cars.reduce((acc ,car,i,array) => {
+    //     acc.push(car.mileage);
+    //     if (i===array.length -1){
+    //         return Math.max.apply(Math, acc);
+    //     } else {acc.push(car.mileage)
+    //     }
+    //     return acc;
+    // },[]);
+
+    console.log(highestmileage);
+
+
+    // TODO: Given the following array, complete the todos...
+    const dogs = [
+        {
+            dogName: 'Bubbles',
+            age: 10,
+            isTrained: false
+        },
+        {
+            dogName: 'Lexie',
+            age: 3,
+            isTrained: true
+        },
+        {
+            dogName: 'Doggy',
+            age: 5,
+            isTrained: false
+        },
+        {
+            dogName: 'Flopper',
+            age: 3,
+            isTrained: true
+        },
+        {
+            dogName: 'Lexie',
+            age: 1,
+            isTrained: true
+        },
+        {
+            dogName: 'Skip',
+            age: 7,
+            isTrained: true
+        },
+        {
+            dogName: 'Blue',
+            age: 4,
+            isTrained: false
+        }
+    ];
+
+    // MAP
+    // TODO 1: using map, create a new array of dog names from the dogs array
+    const dogsNames = dogs.map(e => e.dogName);
+    console.log('Exercise 1');
+    console.log(dogsNames);
+    // TODO 2: using map, create a new array of dog ages from the dogs array
+    const ages = dogs.map(e => e.age);
+    console.log('Exercise 2');
+    console.log(ages);
+    // TODO 3: using map, create a new array of dog objects from the dogs array that only have dog names and age properties and values
+    const dogObject = dogs.map((e) =>{
+        return {
+            dogNames: `${e.dogName}`,
+            age: e.age
+        }
+    });
+    console.log('Exercise 3');
+    console.log(dogObject);
+    // FILTER
+    // TODO 4: using filter, create a new array containing only dogs younger than 10 years old
+    const youngerThan10 = dogs.filter((dog) => dog.age < 10);
+    console.log('Exercise 4');
+    console.log(youngerThan10);
+    // TODO 5: using filter, create a new array containing only dogs named 'Lexie'
+    const lexiDog = dogs.filter((dog => dog.dogName === 'Lexie'));
+    console.log('Exercise 5');
+    console.log(lexiDog);
+    // TODO 6: using filter, create a new array containing only dogs that are trained and younger than 10
+    const youngAndTrained = dogs.filter((dog) => dog.age < 10 && dog.isTrained === true);
+    console.log('Exercise 6');
+    console.log(youngAndTrained);
+    // REDUCE
+    // TODO 7: using reduce, return a string containing all dog names together with no spaces ("BubblesLexieDoggy...")
+    const namesString = dogs.reduce((prev, curr,) =>{
+      return prev + curr.dogName;
+    },'');
+    console.log('Exercise 7:');
+    console.log(namesString);
+    // TODO 8: using reduce, return the total of adding all dog ages together (18)
+    const addAge = dogs.reduce((prev, curr,) =>{
+        return prev + curr.age;
+    },0);
+    console.log('Exercise 8: ');
+    console.log(addAge);
+    // TODO 9: using reduce, return an array of dog objects with all isTrained properties set to true
+    // const trainedDogsFilter = dogs.filter((e) => e.isTrained === true);
+    // console.log(trainedDogsFilter);
+    // const trainedDogs = dogs.reduce((acc,curr ) =>{
+    //     if(curr.isTrained === true){
+    //         console.log(curr)
+    //     }
+    //     return acc
+    // },[]);
+
+    const allGood = dogs.reduce((acc, dog) => {
+        let newDog = {
+            dogname: dog.dogName,
+            age: dog.age,
+            isTrained: true
+        };
+        acc.push(newDog);
+        return acc
+    },[]);
+    console.log(allGood);
+    // EXTRA CHALLENGES
+    // TODO 10: what is the average age of each dog?
+    const averageAge = dogs.reduce((acc, dog,i,array) => {
+        return acc + dog.age/array.length;
+    },0);
+    console.log('Exercise 10:');
+    console.log(averageAge);
+    // TODO 11: what is the average age of dogs that are trained?
+    console.log('Exercise 11:');
+    // TODO 12: what is the average length of names of untrained dogs?
+    console.log('Exercise 12:');
+    // TODO 13: what are the combined ages of all dogs in dog years? (7x more than a human year)
+    let combinedDogYears = dogs.reduce((acc, dog,i) =>{
+        if(i === (dogs.length -1)){
+            return (acc*7)
+        }
+        return acc + parseInt(dog.age);
+    })
+    console.log('Exercise 13:');
+    console.log(combinedDogYears);
+
+    // TODO 14: create a string of the first letters of each dog name for dogs three years old (should be "LF")
+    console.log('Exercise 14:');
--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -39,15 +39,16 @@ const users = [
 // const atLeast3Languages = users.filter((user)=> {
 //     return user.languages.length >= 3
 // });
-const atLeast3Languages = users.filter((user)=> user.languages.length > 3);
+const atLeast3Languages = users.filter((user)=> user.languages.length >= 3);
 
-// console.log(atLeast3Languages);
+console.log(atLeast3Languages);
 
 // const userEmails = users.map((user) => {
 //     return user.email
 // });
+
 const userEmails = users.map((user) => user.email);
-// console.log(userEmails);
+console.log(userEmails);
 
 // let totalYears = users.reduce(function (prev, current ,i,array)  {
 //     return prev + current.yearsOfExperience/ array.length;
@@ -55,7 +56,7 @@ const userEmails = users.map((user) => user.email);
 
 let totalYears = users.reduce((prev, current,i,array ) => prev + current.yearsOfExperience/array.length, 0);
 
-// console.log(totalYears);
+console.log(totalYears);
 
 let longestEmail = users.reduce((result, currentValue) => {
     if(result.length < currentValue.email.length){
@@ -67,11 +68,12 @@ let longestEmail = users.reduce((result, currentValue) => {
 console.log(longestEmail);
 
 const listOfInstructors = users.reduce((acc, current) =>{
-   return acc + current.name +' ';
+    return acc + current.name +' ';
+
 },'Your instructors are: ');
 console.log(listOfInstructors);
 
-const arrayOfLanguages = users.reduce((result, current,i) => {
+const arrayOfLanguages = users.reduce((result, current) => {
         current.languages.forEach((e) =>{
             if(!result.includes(e)){
                 return result.push(e);
@@ -82,7 +84,7 @@ const arrayOfLanguages = users.reduce((result, current,i) => {
 
 console.log(arrayOfLanguages);
 
-//
+
 // let newArray = [];
 // users.forEach((e)=> {
 //     if(!newArray.includes(...e.languages)){
