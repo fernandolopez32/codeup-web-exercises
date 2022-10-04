5335741c77fafc5824cc5c6b2328b05ab65f12d3
Fernando Lopez <flopez00032@gmail.com>
2022-10-04 15:53:33 -0500

    CRUD lecture

--- js/promises.js
+++ js/promises.js
@@ -1,29 +1,52 @@
-$(function (){
-   const gitHubCall = fetch('https://api.github.com/users/fernandolopez32/events/public', {
+// $(function (){
+//    const gitHubCall = fetch('https://api.github.com/users/fernandolopez32/events/public', {
+//         headers: {
+//             'Authorization': 'token' + GIT_HUB_TOKEN
+//         }
+//    }).then(response => response.json())
+//         .then( data => console.log(data[0].created_at.split("T")[0]))
+//         .catch( error => console.error(error));
+
+    // function getLastCommit(username){
+    //     fetch(`https://api.github.com/users/${username}/events/public`, {
+    //         headers: {'Authorization': 'GIT_HUB_TOKEN'}
+    //     }).then( response => response.json())
+    //         .then( username => { console.log(username[0].created_at)
+    //             console.log(username[0].payload.commits[0].message)})
+    //         .catch( error => console.error(error));
+    // }
+    //
+    // console.log(getLastCommit(fernandolopez32))
+    // function getLastCommit(username){
+    //     fetch(`https://api.github.com/users/${username}/events/public`, {
+    //         headers: {'Authorization': 'token' + GIT_HUB_TOKEN}
+    // }).then(respose => respose.json())
+    //         .then(username => {console.log([0].created_at)
+    //
+    //         })
+
+
+// });
+
+
+
+// walk through
+
+function getLastPush(username){
+  return   fetch(`https://api.github.com/users/${username}/events/public`, {
         headers: {
             'Authorization': 'token' + GIT_HUB_TOKEN
         }
-   }).then(response => response.json())
-        .then( data => console.log(data[0].created_at.split("T")[0]))
-        .catch( error => console.error(error));
+    }).then(response => response.json())
+        .then(events => {
+            const dateOfLastPush = new Date(events[0].created_at).toDateString();
+            // console.log(dateOfLastPush)
+            return dateOfLastPush
+        })
+}
 
-    function getLastCommit(username){
-        fetch(`https://api.github.com/users/${username}/events/public`, {
-            headers: {'Authorization': 'GIT_HUB_TOKEN'}
-        }).then( response => response.json())
-            .then( username => { console.log(username[0].created_at)
-                console.log(username[0].payload.commits[0].message)})
-            .catch( error => console.error(error));
-    }
-
-    console.log(getLastCommit(fernandolopez32))
-
-
-});
-
-
-
-
+const lastPushPromise = getLastPush('fernandolopez32');
+lastPushPromise.then(value => console.log(value)).catch(error => console.log(error));
 
 
 
---
+++ js/restful-api.js
@@ -0,0 +1,78 @@
+// API application programing interface
+
+// a set of rules defining how applications and or devices can connect to each other and communicate with one another
+
+//REST means representational state transfer.
+
+// REST API communicate via HTTP request
+
+//In a REST  API, communication via HTTP request is used to perform the standard set of actions called CRUD
+
+//Create, Read, Update, Delete
+
+// JSON JavaScript Object Notation is not mandatory but is popular because it is both human and machine-readable
+
+// https://glitch.com/iron-quirky-laser
+
+//the R in CRUD is Read
+const booksURL = 'https://iron-quirky-laser.glitch.me/books';
+
+const moviesURL = 'https://iron-quirky-laser.glitch.me/movies';
+
+function getMovies(){
+    fetch(moviesURL)
+        .then(resp => resp.json()).then(data => console.log(data));
+}
+getMovies()
+
+// C in CRUD:  Create
+
+const bookToPost = {
+    title: 'Lord Of The Rings',
+    author: {
+        firstName: 'someone',
+        lastName: 'somewhere'
+    }
+}
+
+const postOptions = {
+    method: 'POST',
+    header: {
+        'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify(bookToPost)
+}
+
+function getBooks(){
+    fetch(booksURL)
+        .then(resp => resp.json()).then(data => console.log(data));
+}
+getBooks();
+
+// fetch(booksURL, postOptions).then(getBooks);
+
+// U IN crud: Updating with PUT and PATCH request
+// we use put to replace the entire content
+// we use patch to modify only part of the entry
+
+let modification = {
+    title: 'Elemore of aquetine: queen of France, Queen  of England'
+}
+
+const patchOptions = {
+    method: 'PATCH',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(modification)
+}
+
+// fetch(booksURL + "/1",patchOptions).then(getBooks);
+
+modification = {
+    title: "Elemore of aquetine: and the four kings",
+    author: {
+        firstName: "Amy",
+        lastName:  "kelly"
+    }
+}
